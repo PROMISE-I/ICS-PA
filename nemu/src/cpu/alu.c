200c35ea6def54c629c1addc76485a6fa8936dbf
tracer-pa-public <tracer@njuics.org>
2021-09-26 10:17:58 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:17:58 up 30 days, 23:22,  2 users,  load average: 2.12, 1.54, 1.34 > unitime 1632622677853 > 95df514bf22aefda70800a25256117b56eca8001

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -17,7 +17,7 @@ void set_PF(uint32_t res)
         flag = flag ^ (((res << (31 - i)) & (0x80000000)) >> 31);
     }
     cpu.eflags.PF = flag;
-    printf("res: %x, PF: %x\n", res, cpu.eflags.PF);
+    //printf("res: %x, PF: %x\n", res, cpu.eflags.PF);
 }
 
 void set_ZF(uint32_t res, size_t data_size)
