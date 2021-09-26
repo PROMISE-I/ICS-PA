4750562005022d150ae48d14dd62d2426c9925a0
tracer-pa-public <tracer@njuics.org>
2021-09-26 22:47:52 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:47:52 up 31 days, 11:52,  2 users,  load average: 1.45, 1.39, 1.29 > unitime 1632667672026 > 52240004cf94f2596fd45c96b90307637c6b7302

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -206,8 +206,12 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 	return __ref_alu_imul(src, dest, data_size);
 #else
     //printf("pre: src: %x, dest: %x\n", src, dest);
-	int64_t src_64 = ((int64_t)sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size)) & (0xFFFFFFFFFFFFFFFF >> (64 - data_size));
-	int64_t dest_64 = ((int64_t)sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size)) & (0xFFFFFFFFFFFFFFFF >> (64 - data_size));
+	int64_t src_64 = (int64_t)sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size));
+	int64_t dest_64 = (int64_t)sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	
+	src_64 = src_64 & ((0xFFFFFFFFFFFFFFFF >> (64 - data_size));
+	dest_64 = dest_64 & ((0xFFFFFFFFFFFFFFFF >> (64 - data_size));
+	
 	int64_t res = dest_64 * src_64;
 	//res = res & 0xFFFFFFFF;
     printf("in imul: res: %llx, src_64: %llx, dest_64: %llx, std_res: %llx\n", res, src_64, dest_64, __ref_alu_imul(src, dest, data_size));
