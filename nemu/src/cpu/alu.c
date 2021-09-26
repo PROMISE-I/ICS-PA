a34ce3e10f3ec11cbf74bcd3d453b6d683cf41fc
tracer-pa-public <tracer@njuics.org>
2021-09-26 16:43:29 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:43:28 up 31 days,  5:47,  2 users,  load average: 1.01, 1.26, 1.32 > unitime 1632645808831 > da39c1b9636564689b2aca1c722d0eac4edb501

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -14,7 +14,7 @@ void set_CF_adc(uint32_t res, uint32_t src, uint32_t dest, size_t data_size)
     src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
     dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
     cpu.eflags.CF = (res < src) || (dest != 0 && res == src);
-    printf("in adc: res: %x src: %x, dest: %x, CF: %x\n", res, src, dest, cpu.eflags.CF);
+    //printf("in adc: res: %x src: %x, dest: %x, CF: %x\n", res, src, dest, cpu.eflags.CF);
 }
 
 void set_PF(uint32_t res)
@@ -90,7 +90,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
-    printf("pre-CF: %x\n", cpu.eflags.CF);
+    //printf("pre-CF: %x\n", cpu.eflags.CF);
 	uint32_t res = src + dest + cpu.eflags.CF;
 	
 	set_CF_adc(res, src, dest, data_size);
