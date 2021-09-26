bf3a52bb229853bc09d87b58365fdddfab09da9a
tracer-pa-public <tracer@njuics.org>
2021-09-26 10:02:41 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:02:41 up 30 days, 23:06,  2 users,  load average: 1.21, 1.31, 1.22 > unitime 1632621761249 > 76d403b345223de3da8b5b8f3cda319dfe8c2f7e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -64,13 +64,12 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res = 0;
 	res = src + dest;
 	
+	printf("in add: res: %x, src: %x, dest: %x, CF: %x\n", res, src, dest, cpu.eflags.CF);
 	set_CF_add(res, src, data_size);
 	set_PF(res);
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
 	set_OF_add(res, src, dest, data_size);
-	
-    printf("res: %x, src: %x, dest: %x, CF: %x\n", res, src, dest, cpu.eflags.CF);
 	
 	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
