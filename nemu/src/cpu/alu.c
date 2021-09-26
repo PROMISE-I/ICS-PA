ade4d524ca15aa1d25e254f7e02e281f6fc6e61f
tracer-pa-public <tracer@njuics.org>
2021-09-26 17:04:13 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:04:13 up 31 days,  6:08,  2 users,  load average: 1.31, 1.31, 1.29 > unitime 1632647053327 > bf07d18eda73047eb7fb807bfa59022ea04b1913

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -146,7 +146,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	set_PF(res);
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
-	set_OF_add(res, src, dest, data_size);
+	set_OF_sub(res, src, dest, data_size);
 	//printf("in sub: res: %x, src: %x, dest: %x, OF: %x\n", res, src, dest, cpu.eflags.OF);
 	
 	return res & (0xFFFFFFFF >> (32 - data_size));
