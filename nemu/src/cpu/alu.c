24461df19e309438e2d44989596be06574efcb26
tracer-pa-public <tracer@njuics.org>
2021-09-26 16:39:10 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:39:10 up 31 days,  5:43,  2 users,  load average: 1.72, 1.52, 1.40 > unitime 1632645550807 > ced76748e29766562ee20ad41f370e5db4e46169

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -92,7 +92,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
     printf("pre-CF: %x\n", cpu.eflags.CF);
     uint32_t res1 = src + dest;
     printf("res1: %x\n", res1);
-	uint32_t res = res1 + cpu.eflags.CF;
+	uint32_t res = res1 + (uint32_t)cpu.eflags.CF;
 	
 	set_CF_adc(res, src, dest, data_size);
 	set_PF(res);
