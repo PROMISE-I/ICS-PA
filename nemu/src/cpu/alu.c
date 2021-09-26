fd5c5560350fb23671f01826e3f0f4a1f013da26
tracer-pa-public <tracer@njuics.org>
2021-09-26 16:01:47 +0800

    > clean > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:01:47 up 31 days,  5:06,  2 users,  load average: 1.35, 1.23, 1.20 > unitime > 362c0093bed4cc5bda75c66de72047b7abc39cd

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -8,6 +8,13 @@ void set_CF_add(uint32_t res, uint32_t src, size_t data_size)
     //printf("res: %x, src: %x, CF: %x\n", res, src, cpu.eflags.CF);
 }
 
+void set_CF_adc(uint32_t res, uint32_t src, size_t data_size)
+{
+    res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    cpu.eflags.CF = (res <= src);
+}
+
 void set_PF(uint32_t res)
 {
     uint32_t flag = 1;
@@ -81,10 +88,8 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t src = src + cpu.eflags.CF;
+	return alu_add(src, dest, data_size);
 #endif
 }
 
