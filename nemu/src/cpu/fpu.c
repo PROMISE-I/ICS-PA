9ef304266a1dae11e6bf342b64fc99ec3bcc161c
tracer-pa-public <tracer@njuics.org>
2021-09-29 15:17:13 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:17:13 up 34 days,  4:21,  2 users,  load average: 1.08, 1.19, 1.23 > unitime 1632899833567 > a806bc33477edf163720331c8938c0b78df02ac2

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -302,7 +302,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a)
 	uint32_t exp_res = 0;
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
-	exponent -= 20;
+	exp_res = fa.exponent + fb.exponent - 20 - 127;
 	return internal_normalize(f.sign, exp_res, sig_res);
 }
 
