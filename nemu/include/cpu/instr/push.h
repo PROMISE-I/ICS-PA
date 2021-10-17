68c3782faa2b05bcec91f8a9960ab2f3fd650560
tracer-pa-public <tracer@njuics.org>
2021-10-17 20:40:54 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:40:54 up 52 days,  9:45,  2 users,  load average: 1.89, 2.11, 2.26 > unitime 1634474454740 > 9abc2f8e2af5ed6064777f181dfc7bb680ac41df

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -6,4 +6,5 @@ Put the declarations of `push' instructions here.
 make_instr_func(push_ebp);
 make_instr_func(push_ebx);
 make_instr_func(push_edi);
+make_instr_func(push_rm_v);
 #endif
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -5,7 +5,7 @@ Put the implementations of `push' instructions here.
 make_instr_func(push_ebp){
     OPERAND m;
     
-    cpu.esp += 4;
+    cpu.esp -= 4;
     
     m.type = OPR_MEM;
     m.data_size = 32;
@@ -20,7 +20,7 @@ make_instr_func(push_ebp){
 make_instr_func(push_ebx){
     OPERAND m;
     
-    cpu.esp += 4;
+    cpu.esp -= 4;
     
     m.type = OPR_MEM;
     m.data_size = 32;
@@ -32,17 +32,22 @@ make_instr_func(push_ebx){
     return 1;
 }
 
-make_instr_func(push_edi){
-    OPERAND m;
+make_instr_func(push_rm_v){
+    OPERAND rm, m;
     
-    cpu.esp += 4;
+    int len = 1;
+    cpu.esp -= 4;
+    
+    rm.data_size = data_size;
+    len += modrm_rm(eip+1, &rm);
     
     m.type = OPR_MEM;
     m.data_size = 32;
     m.addr = cpu.esp;
-    m.val = cpu.edi;
     
+    operand_read(&rm);
+    m.val = rm.val;
     operand_write(&m);
     
-    return 1;
+    return len;
 }
