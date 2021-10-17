515664f078a5c618eb5cf9b97a5de03925d44bc6
tracer-pa-public <tracer@njuics.org>
2021-10-17 16:29:07 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:29:07 up 52 days,  5:33,  2 users,  load average: 2.39, 2.39, 2.36 > unitime 1634459347552 > ee84e6a91403c53f193ce2e5f4c37048f1115396

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -2,19 +2,36 @@
 
 make_instr_func(jmp_near)
 {
-        OPERAND rel;
-        rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
-        rel.data_size = data_size;
-        rel.addr = eip + 1;
+    OPERAND rel;
+    rel.type = OPR_IMM;
+    rel.sreg = SREG_CS;
+    rel.data_size = data_size;
+    rel.addr = eip + 1;
 
-        operand_read(&rel);
+    operand_read(&rel);
 
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+    int offset = sign_ext(rel.val, data_size);
+    // thank Ting Xu from CS'17 for finding this bug
+    print_asm_1("jmp", "", 1 + data_size / 8, &rel);
 
-        cpu.eip += offset;
+    cpu.eip += offset;
 
-        return 1 + data_size / 8;
+    return 1 + data_size / 8;
+}
+
+make_instr_func(je_short){
+    OPERAND imm;
+
+    int len = 1;
+    imm.data_size = 16;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += 2;
+    
+    if(cpu.ZF == 0){
+        operand_read(&imm);
+        len += imm.val;
+    }
+    
+    return len;
 }
