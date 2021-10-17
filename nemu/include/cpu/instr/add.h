19922bdcf26dbff2dc4880cf82fa3c2e52a86f8c
tracer-pa-public <tracer@njuics.org>
2021-10-17 16:08:03 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:08:03 up 52 days,  5:12,  2 users,  load average: 2.42, 2.45, 2.31 > unitime 1634458083047 > 319650f43441685d6e19c33840374a31d3e8f80c

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -3,5 +3,5 @@
 /*
 Put the declarations of `add' instructions here.
 */
-
+make_instr_func(add_i2rm_bv);
 #endif
--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -2,3 +2,21 @@
 /*
 Put the implementations of `add' instructions here.
 */
+make_instr_func(add_i2rm_bv){
+    OPERAND imm, rm;
+    
+    int len = 1;
+    rm.data_size = data_size; 
+    len += modrm_rm(eip+1, &rm);
+    
+    imm.type = OPR_IMM;
+    imm.addr = eip + len;
+    imm.data_size = 8;
+    
+    operand_read(&rm);
+    operand_read(&imm);
+    rm.val += imm.val;
+    operand_write(&rm);
+    
+    return len + 1;
+}
