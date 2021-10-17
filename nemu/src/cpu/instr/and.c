0e6739a8ab4bc854ac0644adf1444a99a55f9d96
tracer-pa-public <tracer@njuics.org>
2021-10-17 20:26:27 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:26:27 up 52 days,  9:30,  2 users,  load average: 2.39, 2.42, 2.42 > unitime 1634473587104 > 6920ee0eaae57d101db764d9ecab409ba3c1a965

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -2,3 +2,22 @@
 /*
 Put the implementations of `and' instructions here.
 */
+make_instr_func(and_i2rm_bv){
+    OPERAND imm, rm;
+    
+    int len = 1;
+    rm.data_size = data_size;
+    len += modrm_rm(eip+1, &rm);
+    
+    imm.type = OPR_IMM;
+    imm.data_size = 8;
+    imm.addr = eip + len;
+    
+    operand_read(&rm);
+    operand_read(&imm);
+    imm.val = sign_ext(imm, 8);
+    rm.val = alu_and(imm.val, rm.val);
+    operand_write(&rm);
+    
+    return len + 1;
+}
