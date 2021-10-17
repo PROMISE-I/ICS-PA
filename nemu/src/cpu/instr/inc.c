74f6e21dc94d768b396498165856aa560711de66
tracer-pa-public <tracer@njuics.org>
2021-10-17 19:44:33 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:44:33 up 52 days,  8:48,  2 users,  load average: 2.29, 2.33, 2.37 > unitime 1634471073584 > dc902f54744458efba3a8afaa66d0ec8baf175b

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,3 +2,16 @@
 /*
 Put the implementations of `inc' instructions here.
 */
+make_instr_func(inc_rm_v){
+    OPERAND rm;
+    
+    int len = 1;
+    rm.data_size = data_size;
+    len += modrm_rm(eip+1, &rm);
+    
+    operand_read(&rm);
+    rm.val++;
+    operand_write(&rm);
+    
+    return len;
+}
