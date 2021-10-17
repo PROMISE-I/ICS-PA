bf882d153866b73d8ffafdea8376b14c64ceb5aa
tracer-pa-public <tracer@njuics.org>
2021-10-17 12:10:49 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:10:49 up 52 days,  1:15,  2 users,  load average: 2.35, 2.36, 2.36 > unitime 1634443849201 > 62f376dab58a0b71d15b42957485ba1c2109b25c

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -5,13 +5,14 @@ Put the implementations of `push' instructions here.
 make_instr_func(push_ebp){
     OPERAND m;
     
+    cpu.esp += 4;
+    
     m.type = OPR_MEM;
     m.data_size = 32;
     m.addr = cpu.esp;
     m.val = cpu.ebp;
     
     operand_write(&m);
-    cpu.esp += 4;
     
     return 1;
 }
