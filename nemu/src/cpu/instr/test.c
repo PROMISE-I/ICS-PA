d6ec62cdc3056a05e14e9fc390c22b9b0f03b611
tracer-pa-public <tracer@njuics.org>
2021-10-17 16:16:46 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:16:46 up 52 days,  5:21,  2 users,  load average: 2.11, 2.31, 2.29 > unitime 1634458606303 > ae5695ea5a1e45aa68eb13d0fba86ea84f290fff

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -2,3 +2,15 @@
 /*
 Put the implementations of `test' instructions here.
 */
+make_instr_func(test_r2rm_v){
+    OPERAND r, rm;
+    
+    int len = 1;
+    r.data_size = data_size;
+    rm.data_size = data_size;
+    len += modrm_r_rm(eip + 1, &r, &rm);
+    
+    alu_and(r.val, rm.val);
+    
+    return len;
+}
