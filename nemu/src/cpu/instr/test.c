c72bdfd276e0f3e2b767ed88e5090c3b1b0dd52e
tracer-pa-public <tracer@njuics.org>
2021-10-17 17:15:15 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:15:15 up 52 days,  6:19,  2 users,  load average: 2.58, 2.36, 2.35 > unitime 1634462115479 > 1e369e2b74a5f7d54948252327d8432e62bea20

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -10,8 +10,10 @@ make_instr_func(test_r2rm_v){
     rm.data_size = data_size;
     len += modrm_r_rm(eip + 1, &r, &rm);
     
+    operand_read(&r);
+    operand_read(&rm);
     alu_and(r.val, rm.val, data_size);
 
-    printf("ZF: %x, r.val: %x, rm.val: %x\n", cpu.eflags.ZF, r.val, rm.val);
+    //printf("ZF: %x, r.val: %x, rm.val: %x\n", cpu.eflags.ZF, r.val, rm.val);
     return len;
 }
