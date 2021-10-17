560b29bb3abc775d05b88e951ae935ed120645e5
tracer-pa-public <tracer@njuics.org>
2021-10-17 19:00:03 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:00:03 up 52 days,  8:04,  2 users,  load average: 3.15, 2.51, 2.42 > unitime 1634468402836 > 46b8b3301a1be129bebfb59c13269cc2bb85c9f3

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -2,3 +2,22 @@
 /*
 Put the implementations of `sub' instructions here.
 */
+make_instr_func(sub_i2rm_bv){
+    OPERAND imm, rm;
+    
+    int len = 1;
+    rm.data_size = data_size;
+    len += modrm_rm(eip+1, &rm);
+    
+    imm.type = OPR_IMM;
+    imm.data_size = 8;
+    imm.addr = eip + len;
+    len += 1;
+    
+    operand_read(&rm);
+    operand_read(&imm);
+    rm.val = alu.sub(sign_ext(imm.val, 8), rm.val, data_size);
+    operand_write(&rm);
+    
+    return len;
+}
