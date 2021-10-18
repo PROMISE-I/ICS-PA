b5ab3d4c0808231297fd89b3a005d3a5bbdcfdc4
tracer-pa-public <tracer@njuics.org>
2021-10-18 17:50:48 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:50:48 up 53 days,  6:55,  2 users,  load average: 2.17, 2.59, 2.56 > unitime 1634550647920 > 54411db72c9f35216782e22cabba6de1b33fabc4

--- nemu/include/cpu/instr/pop.h
+++ nemu/include/cpu/instr/pop.h
@@ -3,6 +3,12 @@
 /*
 Put the declarations of `pop' instructions here.
 */
+make_instr_func(pop_eax);
+make_instr_func(pop_ecx);
+make_instr_func(pop_edx);
 make_instr_func(pop_ebx);
+make_instr_func(pop_esp);
 make_instr_func(pop_ebp);
+make_instr_func(pop_esi);
+make_instr_func(pop_edi);
 #endif
--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -2,6 +2,48 @@
 /*
 Put the implementations of `pop' instructions here.
 */
+make_instr_func(pop_eax){
+    OPERAND m;
+    
+    m.type = OPR_MEM;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    
+    operand_read(&m);
+    cpu.eax = m.val;
+    cpu.esp += 4;
+    
+    return 1;
+}
+
+make_instr_func(pop_ecx){
+    OPERAND m;
+    
+    m.type = OPR_MEM;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    
+    operand_read(&m);
+    cpu.ecx = m.val;
+    cpu.esp += 4;
+    
+    return 1;
+}
+
+make_instr_func(pop_edx){
+    OPERAND m;
+    
+    m.type = OPR_MEM;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    
+    operand_read(&m);
+    cpu.edx = m.val;
+    cpu.esp += 4;
+    
+    return 1;
+}
+
 make_instr_func(pop_ebx){
     OPERAND m;
     
@@ -16,6 +58,20 @@ make_instr_func(pop_ebx){
     return 1;
 }
 
+make_instr_func(pop_esp){
+    OPERAND m;
+    
+    m.type = OPR_MEM;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    
+    operand_read(&m);
+    cpu.esp = m.val;
+    //pop esp 后还要esp+=4？
+    
+    return 1;
+}
+
 make_instr_func(pop_ebp){
     OPERAND m;
     
@@ -25,6 +81,34 @@ make_instr_func(pop_ebp){
     
     operand_read(&m);
     cpu.ebp = m.val;
+    cpu.esp += 4;
+    
+    return 1;
+}
+
+make_instr_func(pop_esi){
+    OPERAND m;
+    
+    m.type = OPR_MEM;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    
+    operand_read(&m);
+    cpu.esi = m.val;
+    cpu.esp += 4;
+    
+    return 1;
+}
+
+make_instr_func(pop_edi){
+    OPERAND m;
+    
+    m.type = OPR_MEM;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    
+    operand_read(&m);
+    cpu.edi = m.val;
     cpu.esp += 4;
     
     return 1;
