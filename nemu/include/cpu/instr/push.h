d778704bbfee4695ca2518cb0c8451dd31f80c2f
tracer-pa-public <tracer@njuics.org>
2021-10-18 17:02:09 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:02:09 up 53 days,  6:06,  2 users,  load average: 2.12, 2.38, 2.42 > unitime 1634547729581 > 67a3848a77bc92ceccea11481bc7dba89842038

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -7,7 +7,7 @@ make_instr_func(push_eax);
 make_instr_func(push_ecx);
 make_instr_func(push_edx);
 make_instr_func(push_ebx);
-make_instr_func(push_esb);
+make_instr_func(push_esp);
 make_instr_func(push_ebp);
 make_instr_func(push_esi);
 make_instr_func(push_edi);
