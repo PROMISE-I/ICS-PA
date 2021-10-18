826a6e8d19d28dd88729302ba6f933de22abbcc2
tracer-pa-public <tracer@njuics.org>
2021-10-18 18:00:46 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:00:46 up 53 days,  7:05,  2 users,  load average: 2.18, 2.40, 2.50 > unitime 1634551245930 > cac8b3e7f8041579120fab47eac0d9cfcf1f778a

--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -2,3 +2,66 @@
 /*
 Put the implementations of `or' instructions here.
 */
+make_instr_func(xor_r2rm_b){
+    OPERAND r, rm;
+    
+    int len = 1;
+    r.data_size = 8;
+    rm.data_size = 8;
+    len += modrm_r_rm(eip+1, &r, &rm);
+    
+    operand_read(&r);
+    operand_read(&rm);
+    rm.val = alu_or(r.val, rm.val, 8);
+    operand_write(&rm);
+    
+    return len;
+}
+
+make_instr_func(xor_r2rm_v){
+    OPERAND r, rm;
+    
+    int len = 1;
+    r.data_size = data_size;
+    rm.data_size = data_size;
+    len += modrm_r_rm(eip+1, &r, &rm);
+    
+    operand_read(&r);
+    operand_read(&rm);
+    rm.val = alu_or(r.val, rm.val, data_size);
+    operand_write(&rm);
+    
+    return len;
+}
+
+make_instr_func(xor_rm2r_b){
+    OPERAND r, rm;
+    
+    int len = 1;
+    r.data_size = 8;
+    rm.data_size = 8;
+    len += modrm_r_rm(eip+1, &r, &rm);
+    
+    operand_read(&r);
+    operand_read(&rm);
+    r.val = alu_or(r.val, rm.val, 8);
+    operand_write(&r);
+    
+    return len;
+}
+
+make_instr_func(xor_rm2r_v){
+    OPERAND r, rm;
+    
+    int len = 1;
+    r.data_size = data_size;
+    rm.data_size = data_size;
+    len += modrm_r_rm(eip+1, &r, &rm);
+    
+    operand_read(&r);
+    operand_read(&rm);
+    r.val = alu_or(r.val, rm.val, data_size);
+    operand_write(&r);
+    
+    return len;
+}
