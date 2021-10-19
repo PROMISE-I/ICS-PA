02a2369241545064b931be22f0813f9164ac19d8
tracer-pa-public <tracer@njuics.org>
2021-10-19 19:53:11 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:53:11 up 54 days,  8:57,  3 users,  load average: 2.90, 2.49, 2.31 > unitime 1634644391346 > a0560ed7f83432a91e9d096e976c7c04e2f785c0

--- nemu/include/cpu/instr/inc.h
+++ nemu/include/cpu/instr/inc.h
@@ -4,4 +4,12 @@
 Put the declarations of `inc' instructions here.
 */
 make_instr_func(inc_rm_v);
+make_instr_func(inc_eax);
+make_instr_func(inc_ecx);
+make_instr_func(inc_edx);
+make_instr_func(inc_ebx);
+make_instr_func(inc_esp);
+make_instr_func(inc_ebp);
+make_instr_func(inc_esi);
+make_instr_func(inc_edi);
 #endif
--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -15,3 +15,43 @@ make_instr_func(inc_rm_v){
     
     return len;
 }
+
+make_instr_func(inc_eax){
+    cpu.eax = alu_add(1, cpu.eax, 32);
+    return 1;
+}
+
+make_instr_func(inc_ecx){
+    cpu.ecx = alu_add(1, cpu.ecx, 32);
+    return 1;
+}
+
+make_instr_func(inc_edx){
+    cpu.edx = alu_add(1, cpu.edx, 32);
+    return 1;
+}
+
+make_instr_func(inc_ebx){
+    cpu.ebx = alu_add(1, cpu.ebx, 32);
+    return 1;
+}
+
+make_instr_func(inc_esp){
+    cpu.esp = alu_add(1, cpu.esp, 32);
+    return 1;
+}
+
+make_instr_func(inc_ebp){
+    cpu.ebp = alu_add(1, cpu.ebp, 32);
+    return 1;
+}
+
+make_instr_func(inc_esi){
+    cpu.esi = alu_add(1, cpu.esi, 32);
+    return 1;
+}
+
+make_instr_func(inc_edi){
+    cpu.edi = alu_add(1, cpu.edi, 32);
+    return 1;
+}
