5be82368963eeda710a0ed9a6db0349a70e9e3c0
tracer-pa-public <tracer@njuics.org>
2021-10-19 20:53:12 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:53:12 up 54 days,  9:57,  3 users,  load average: 2.22, 2.31, 2.30 > unitime 1634647992460 > a2aa57131649c726504c0209c183649d970a1d29

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -296,19 +296,280 @@ make_instr_func(jnle_short)
     return len;
 }
 
+
+//two opcodes
+
+make_instr_func(jo_v)
+{
+    OPERAND imm;
+
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    
+    if(cpu.eflags.OF == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jno_v)
+{
+    OPERAND imm;
+
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    
+    if(cpu.eflags.OF == 0){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jb_v)
+{
+    OPERAND imm;
+
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    
+    if(cpu.eflags.CF == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jnb_v)
+{
+    OPERAND imm;
+
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    
+    if(cpu.eflags.CF == 0){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jz_v)
+{
+    OPERAND imm;
+
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    
+    if(cpu.eflags.ZF == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jnz_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if(cpu.eflags.ZF == 0){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
 make_instr_func(jbe_v)
 {
     OPERAND imm;
     
     int len = 1;
-
-    imm.type = OPR_IMM;
     imm.data_size = data_size;
+    imm.type = OPR_IMM;
     imm.addr = eip + 1;
-    operand_read(&imm);
-    len += data_size / 8;
-    
+    len += data_size/8;
     if((cpu.eflags.CF | cpu.eflags.ZF) == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jnbe_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if((cpu.eflags.CF | cpu.eflags.ZF) == 0){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(js_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if(cpu.eflags.SF == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jns_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if(cpu.eflags.SF == 0){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jp_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if(cpu.eflags.PF == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jnp_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if(cpu.eflags.PF == 0){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jl_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if((cpu.eflags.SF ^ cpu.eflags.OF) == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jnl_short)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if((cpu.eflags.SF ^ cpu.eflags.OF) == 0){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;  
+}
+
+make_instr_func(jle_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if(((cpu.eflags.SF ^ cpu.eflags.OF) | cpu.eflags.ZF) == 1){
+        operand_read(&imm);
+        len += sign_ext(imm.val, data_size);
+    }
+    
+    return len;
+}
+
+make_instr_func(jnle_v)
+{
+    OPERAND imm;
+    
+    int len = 1;
+    imm.data_size = data_size;
+    imm.type = OPR_IMM;
+    imm.addr = eip + 1;
+    len += data_size/8;
+    if(((cpu.eflags.SF ^ cpu.eflags.OF) | cpu.eflags.ZF) == 0){
+        operand_read(&imm);
         len += sign_ext(imm.val, data_size);
     }
     
