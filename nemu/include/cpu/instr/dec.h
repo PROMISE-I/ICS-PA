de3d45889b8b62fe126f1ace7a37a3371f60ca1e
tracer-pa-public <tracer@njuics.org>
2021-10-19 19:45:02 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:45:02 up 54 days,  8:49,  3 users,  load average: 2.27, 2.18, 2.22 > unitime 1634643902228 > daddbc9fe938331fbdbc9d69df35a634eae24ba

--- nemu/include/cpu/instr/dec.h
+++ nemu/include/cpu/instr/dec.h
@@ -3,5 +3,12 @@
 /*
 Put the declarations of `dec' instructions here.
 */
-
+make_instr_func(dec_eax);
+make_instr_func(dec_ecx);
+make_instr_func(dec_edx);
+make_instr_func(dec_ebx);
+make_instr_func(dec_esp);
+make_instr_func(dec_ebp);
+make_instr_func(dec_esi);
+make_instr_func(dec_edi);
 #endif
--- nemu/src/cpu/instr/dec.c
+++ nemu/src/cpu/instr/dec.c
@@ -2,3 +2,42 @@
 /*
 Put the implementations of `dec' instructions here.
 */
+make_instr_func(dec_eax){
+    cpu.eax = alu_sub(1, cpu.eax, 32);
+    return 1;
+}
+
+make_instr_func(dec_ecx){
+    cpu.ecx = alu_sub(1, cpu.ecx, 32);
+    return 1;
+}
+
+make_instr_func(dec_edx){
+    cpu.edx = alu_sub(1, cpu.edx, 32);
+    return 1;
+}
+
+make_instr_func(dec_ebx){
+    cpu.ebx = alu_sub(1, cpu.ebx, 32);
+    return 1;
+}
+
+make_instr_func(dec_esp){
+    cpu.esp = alu_sub(1, cpu.esp, 32);
+    return 1;
+}
+
+make_instr_func(dec_ebp){
+    cpu.ebp = alu_sub(1, cpu.ebp, 32);
+    return 1;
+}
+
+make_instr_func(dec_esi){
+    cpu.esi = alu_sub(1, cpu.esi, 32);
+    return 1;
+}
+
+make_instr_func(dec_edi){
+    cpu.edi = alu_sub(1, cpu.edi, 32);
+    return 1;
+}
