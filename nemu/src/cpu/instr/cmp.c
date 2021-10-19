da0ca8a293c10fdef252d3d2b66c633f0f1aa27f
tracer-pa-public <tracer@njuics.org>
2021-10-19 18:52:41 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:52:41 up 54 days,  7:56,  2 users,  load average: 2.36, 2.40, 2.37 > unitime 1634640760979 > ac01aefc312dcbc5dff875935130124447e861e0

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -2,6 +2,43 @@
 /*
 Put the implementations of `cmp' instructions here.
 */
+make_instr_func(cmp_i2rm_b){
+    OPERAND imm, rm;
+    
+    int len = 1;
+    rm.data_size = 8;
+    len += modrm_rm(eip + 1, &rm);
+    
+    imm.type = OPR_IMM;
+    imm.addr = eip + len;
+    imm.data_size = 8;
+    
+    operand_read(&imm);
+    imm.val = sign_ext(imm.val, data_size);
+    operand_read(&rm);
+    alu_sub(imm.val, rm.val, data_size);
+    
+    return len + 1;
+}
+
+make_instr_func(cmp_i2rm_v){
+    OPERAND imm, rm;
+    
+    int len = 1;
+    rm.data_size = data_size;
+    len += modrm_rm(eip + 1, &rm);
+    
+    imm.type = OPR_IMM;
+    imm.addr = eip + len;
+    imm.data_size = data_size;
+    
+    operand_read(&imm);
+    imm.val = sign_ext(imm.val, data_size);
+    operand_read(&rm);
+    alu_sub(imm.val, rm.val, data_size);
+    
+    return len + data_size/8;
+}
 
 make_instr_func(cmp_i2rm_bv){
     OPERAND imm, rm;
