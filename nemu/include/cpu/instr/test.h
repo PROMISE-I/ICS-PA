698a2ea7414abc90a751777590596cfb0f4e9d61
tracer-pa-public <tracer@njuics.org>
2021-10-22 10:17:17 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:17:17 up 56 days, 23:21,  3 users,  load average: 2.34, 2.43, 2.45 > unitime 1634869037970 > f3b76126fa138e6195c6f627b165f252e871148

--- nemu/include/cpu/instr/test.h
+++ nemu/include/cpu/instr/test.h
@@ -7,4 +7,6 @@ make_instr_func(test_r2rm_b);
 make_instr_func(test_r2rm_v);
 make_instr_func(test_i2rm_b);
 make_instr_func(test_i2rm_v);
+make_instr_func(test_i2a_b);
+make_instr_func(test_i2a_v);
 #endif
