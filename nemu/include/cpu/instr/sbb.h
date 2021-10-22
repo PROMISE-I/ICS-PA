cc915f8de7baee670e44911b83bcf4e53b72c9da
tracer-pa-public <tracer@njuics.org>
2021-10-22 09:22:51 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:22:51 up 56 days, 22:27,  3 users,  load average: 2.41, 2.48, 2.46 > unitime 1634865771079 > c86da04353971c3c23657591a301664f16122b27

--- nemu/include/cpu/instr/sbb.h
+++ nemu/include/cpu/instr/sbb.h
@@ -3,5 +3,10 @@
 /*
 Put the declarations of `sbb' instructions here.
 */
-
+make_instr_func(sbb_r2rm_b);
+make_instr_func(sbb_r2rm_v);
+make_instr_func(sbb_rm2r_b);
+make_instr_func(sbb_rm2r_v);
+make_instr_func(sbb_i2a_b);
+make_instr_func(sbb_i2a_v);
 #endif
--- nemu/src/cpu/instr/sbb.c
+++ nemu/src/cpu/instr/sbb.c
@@ -2,3 +2,16 @@
 /*
 Put the implementations of `sbb' instructions here.
 */
+instr_execute_2op{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_dest.val = alu_sbb(opr_src.val, opr_dest.val, opr_src.data_size);
+    operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(sbb, r, rm, b)
+make_instr_impl_2op(sbb, r, rm, v)
+make_instr_impl_2op(sbb, rm, r, b)
+make_instr_impl_2op(sbb, rm, r, v)
+make_instr_impl_2op(sbb, i, a, b)
+make_instr_impl_2op(sbb, i, a, v)
