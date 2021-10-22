556bb5785731b56e0e077e2c9a6f8aba933d8433
tracer-pa-public <tracer@njuics.org>
2021-10-22 09:47:18 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:47:18 up 56 days, 22:51,  3 users,  load average: 2.39, 2.30, 2.39 > unitime 1634867238181 > 6ccbb8c2206e730cfd2c3de5d9c720982ef6eb79

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -85,3 +85,13 @@ make_instr_func(sub_i2rm_bv){
     
     return len;
 }
+
+static void instr_execute_2op(){
+    operand_read(&opr_src);
+    opreand_read(&opr_dest);
+    opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_src.data_size);
+    operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(sub, i, rm, v)
+make_instr_impl_2op(sub, i, rm, b)
