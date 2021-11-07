59b32ddc7f52584b3d608a573d777d51ed673f87
tracer-pa-public <tracer@njuics.org>
2021-11-07 17:23:59 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:23:59 up 73 days,  6:28,  4 users,  load average: 2.64, 2.46, 2.45 > unitime 1636277039219 > e30dde022eb132f64df6049fe100f09c3e003bb5

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -15,7 +15,7 @@ make_instr_func(add_r2rm_b){
     rm.val = alu_add(r.val, rm.val, 8);
     operand_write(&rm);
     
-    print_asm_2("add", "b", &r, &rm);
+    print_asm_2("add", "b", len, &r, &rm);
     return len;
 }
 
@@ -32,7 +32,7 @@ make_instr_func(add_r2rm_v){
     rm.val = alu_add(r.val, rm.val, data_size);
     operand_write(&rm);
     
-    print_asm_2("add", "v", &r, &rm);
+    print_asm_2("add", "v", len, &r, &rm);
     return len;
 }
 
@@ -52,7 +52,7 @@ make_instr_func(add_i2rm_v){
     rm.val = alu_add(imm.val, rm.val, data_size);
     operand_write(&rm);
     
-    print_asm_2("add", "v", &imm, &rm);
+    print_asm_2("add", "v", len, &imm, &rm);
     return len + data_size/8;
 }
 
@@ -72,7 +72,7 @@ make_instr_func(add_i2rm_bv){
     rm.val = alu_add(sign_ext(imm.val, 8), rm.val, data_size);
     operand_write(&rm);
     
-    print_asm_2("add", "bv", &imm, &rm);
+    print_asm_2("add", "bv", len, &imm, &rm);
     return len + 1;
 }
 
@@ -87,6 +87,6 @@ make_instr_func(add_i2a_v){
     operand_read(&imm);
     cpu.eax = alu_add(cpu.eax, sign_ext(imm.val, data_size), 32);
     
-    print_asm_1("add", "v", &imm);
+    print_asm_1("add", "v", len, &imm);
     return len + data_size/8;
 }
