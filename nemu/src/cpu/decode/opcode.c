26bc70e698d54a5c17816e375ae693bd9a9dfe51
tracer-pa-public <tracer@njuics.org>
2021-11-07 11:46:04 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:46:04 up 73 days, 50 min,  4 users,  load average: 2.56, 2.50, 2.51 > unitime 1636256764627 > 35fa791d46d59c1771e5ed70dce289996229331

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -64,7 +64,7 @@ instr_func opcode_entry[256] = {
     /* 0xf0 - 0xf3*/ inv, break_point, inv, rep_repe,
     /* 0xf4 - 0xf7*/ hlt, inv, group_3_b, group_3_v,
     /* 0xf8 - 0xfb*/ clc, inv, inv, sti,
-    /* 0xfc - 0xff*/ cld, inv, std, group_5_indirect,
+    /* 0xfc - 0xff*/ cld, std, inv, group_5_indirect,
 };
 
 /* 0x80 */
