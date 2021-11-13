6942f29f5bacfcf45af164e584e29ebc7b507f2a
tracer-pa-public <tracer@njuics.org>
2021-11-13 18:52:43 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:52:43 up 79 days,  7:56,  4 users,  load average: 2.32, 2.41, 2.58 > unitime 1636800763058 > 2deda4373286bc9aaa907539937f29745ddd7b56

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -4,7 +4,7 @@ Put the implementations of `xor' instructions here.
 */
 static void instr_execute_2op()
 {
-    oeprand_read(&opr_src);
+    operand_read(&opr_src);
     operand_read(&opr_dest);
     opr_dest.val = alu_xor(opr_src.val, opr_dest.val, opr_src.data_size);
     operand_write(&opr_dest);
