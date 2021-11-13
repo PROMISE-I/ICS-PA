cb6ec55184340bf7e5a5a7eaaf5dc6263f230f51
tracer-pa-public <tracer@njuics.org>
2021-11-13 18:52:23 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:52:23 up 79 days,  7:56,  4 users,  load average: 2.47, 2.45, 2.60 > unitime 1636800742959 > 16b91727e3216428e79a3e7f9a4c3d5a9b2cca83

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -2,66 +2,79 @@
 /*
 Put the implementations of `xor' instructions here.
 */
-make_instr_func(xor_r2rm_b){
-    OPERAND r, rm;
-    
-    int len = 1;
-    r.data_size = 8;
-    rm.data_size = 8;
-    len += modrm_r_rm(eip+1, &r, &rm);
-    
-    operand_read(&r);
-    operand_read(&rm);
-    rm.val = alu_xor(r.val, rm.val, 8);
-    operand_write(&rm);
-    
-    return len;
+static void instr_execute_2op()
+{
+    oeprand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_dest.val = alu_xor(opr_src.val, opr_dest.val, opr_src.data_size);
+    operand_write(&opr_dest);
 }
 
-make_instr_func(xor_r2rm_v){
-    OPERAND r, rm;
-    
-    int len = 1;
-    r.data_size = data_size;
-    rm.data_size = data_size;
-    len += modrm_r_rm(eip+1, &r, &rm);
-    
-    operand_read(&r);
-    operand_read(&rm);
-    rm.val = alu_xor(r.val, rm.val, data_size);
-    operand_write(&rm);
-    
-    return len;
-}
+make_instr_impl_2op(xor, r, rm, b)
+make_instr_impl_2op(xor, r, rm, v)
+make_instr_impl_2op(xor, rm, r, b)
+make_instr_impl_2op(xor, rm, r, v)
 
-make_instr_func(xor_rm2r_b){
-    OPERAND r, rm;
+// make_instr_func(xor_r2rm_b){
+//     OPERAND r, rm;
     
-    int len = 1;
-    r.data_size = 8;
-    rm.data_size = 8;
-    len += modrm_r_rm(eip+1, &r, &rm);
+//     int len = 1;
+//     r.data_size = 8;
+//     rm.data_size = 8;
+//     len += modrm_r_rm(eip+1, &r, &rm);
     
-    operand_read(&r);
-    operand_read(&rm);
-    r.val = alu_xor(r.val, rm.val, 8);
-    operand_write(&r);
+//     operand_read(&r);
+//     operand_read(&rm);
+//     rm.val = alu_xor(r.val, rm.val, 8);
+//     operand_write(&rm);
     
-    return len;
-}
+//     return len;
+// }
 
-make_instr_func(xor_rm2r_v){
-    OPERAND r, rm;
+// make_instr_func(xor_r2rm_v){
+//     OPERAND r, rm;
     
-    int len = 1;
-    r.data_size = data_size;
-    rm.data_size = data_size;
-    len += modrm_r_rm(eip+1, &r, &rm);
+//     int len = 1;
+//     r.data_size = data_size;
+//     rm.data_size = data_size;
+//     len += modrm_r_rm(eip+1, &r, &rm);
     
-    operand_read(&r);
-    operand_read(&rm);
-    r.val = alu_xor(r.val, rm.val, data_size);
-    operand_write(&r);
+//     operand_read(&r);
+//     operand_read(&rm);
+//     rm.val = alu_xor(r.val, rm.val, data_size);
+//     operand_write(&rm);
     
-    return len;
-}
+//     return len;
+// }
+
+// make_instr_func(xor_rm2r_b){
+//     OPERAND r, rm;
+    
+//     int len = 1;
+//     r.data_size = 8;
+//     rm.data_size = 8;
+//     len += modrm_r_rm(eip+1, &r, &rm);
+    
+//     operand_read(&r);
+//     operand_read(&rm);
+//     r.val = alu_xor(r.val, rm.val, 8);
+//     operand_write(&r);
+    
+//     return len;
+// }
+
+// make_instr_func(xor_rm2r_v){
+//     OPERAND r, rm;
+    
+//     int len = 1;
+//     r.data_size = data_size;
+//     rm.data_size = data_size;
+//     len += modrm_r_rm(eip+1, &r, &rm);
+    
+//     operand_read(&r);
+//     operand_read(&rm);
+//     r.val = alu_xor(r.val, rm.val, data_size);
+//     operand_write(&r);
+    
+//     return len;
+// }
