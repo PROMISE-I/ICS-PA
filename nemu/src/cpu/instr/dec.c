a0e79978068e50888d0ad68683d0356dab6680f6
tracer-pa-public <tracer@njuics.org>
2021-11-13 11:40:54 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:40:54 up 79 days, 45 min,  4 users,  load average: 2.77, 2.56, 2.53 > unitime 1636774854650 > 51acd3ef554a8c9a5107c0911558906353601cf0

--- nemu/src/cpu/instr/dec.c
+++ nemu/src/cpu/instr/dec.c
@@ -5,7 +5,7 @@ Put the implementations of `dec' instructions here.
 static void instr_execute_1op()
 {
     operand_read(&opr_src);
-    opr_src.val = alu_sub(1, opr_src, opr_src.data_size);
+    opr_src.val = alu_sub(1, opr_src.val, opr_src.data_size);
     operand_write(&opr_src);
 }
 
