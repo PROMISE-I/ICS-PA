3c1ce80c08a8d53a6b5bedad327a3863fc0ad08d
tracer-pa-public <tracer@njuics.org>
2021-11-13 11:22:38 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:22:38 up 79 days, 26 min,  4 users,  load average: 2.17, 2.51, 2.52 > unitime 1636773758054 > b8924d562d6dbf60e6b857bef39a8dc935b4e6c5

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -6,11 +6,11 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_dest.val = alu_adc(opr_src.val, opr_dest.val, data_size);
+    opr_dest.val = alu_adc(opr_src.val, opr_dest.val, opr_src.data_size);
     operand_write(&opr_dest);
 }
 
-make_instr_impl_2op(adc, r, rm, v);
+make_instr_impl_2op(adc, r, rm, v)
 
 // make_instr_func(adc_r2rm_v){
 //     OPERAND r, rm;
--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -2,59 +2,72 @@
 /*
 Put the implementations of `add' instructions here.
 */
-make_instr_func(add_r2rm_b){
-    OPERAND r, rm;
-    
-    int len = 1;
-    rm.data_size = 8; 
-    r.data_size = 8;
-    len += modrm_r_rm(eip+1, &r, &rm);
-    
-    operand_read(&rm);
-    operand_read(&r);
-    rm.val = alu_add(r.val, rm.val, 8);
-    operand_write(&rm);
-    
-    print_asm_2("add", "b", len, &r, &rm);
-    return len;
+static void instr_execute_2op()
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_dest.val = alu_add(opr_src.val, opr_dest.val, opr_src.data_size);
+    operand_write(&opr_dest);
 }
 
-make_instr_func(add_r2rm_v){
-    OPERAND r, rm;
-    
-    int len = 1;
-    rm.data_size = data_size; 
-    r.data_size = data_size;
-    len += modrm_r_rm(eip+1, &r, &rm);
-    
-    operand_read(&rm);
-    operand_read(&r);
-    rm.val = alu_add(r.val, rm.val, data_size);
-    operand_write(&rm);
-    
-    print_asm_2("add", "v", len, &r, &rm);
-    return len;
-}
+make_instr_impl_2op(add, r, rm, b)
+make_instr_impl_2op(add, r, rm, v)
+make_instr_impl_2op(add, i, rm, v)
+make_instr_impl_2op(add, i, a, v)
 
-make_instr_func(add_i2rm_v){
-    OPERAND imm, rm;
+// make_instr_func(add_r2rm_b){
+//     OPERAND r, rm;
     
-    int len = 1;
-    rm.data_size = data_size; 
-    len += modrm_rm(eip+1, &rm);
+//     int len = 1;
+//     rm.data_size = 8; 
+//     r.data_size = 8;
+//     len += modrm_r_rm(eip+1, &r, &rm);
     
-    imm.type = OPR_IMM;
-    imm.addr = eip + len;
-    imm.data_size = data_size;
+//     operand_read(&rm);
+//     operand_read(&r);
+//     rm.val = alu_add(r.val, rm.val, 8);
+//     operand_write(&rm);
     
-    operand_read(&rm);
-    operand_read(&imm);
-    rm.val = alu_add(imm.val, rm.val, data_size);
-    operand_write(&rm);
+//     print_asm_2("add", "b", len, &r, &rm);
+//     return len;
+// }
+
+// make_instr_func(add_r2rm_v){
+//     OPERAND r, rm;
     
-    print_asm_2("add", "v", len, &imm, &rm);
-    return len + data_size / 8;
-}
+//     int len = 1;
+//     rm.data_size = data_size; 
+//     r.data_size = data_size;
+//     len += modrm_r_rm(eip+1, &r, &rm);
+    
+//     operand_read(&rm);
+//     operand_read(&r);
+//     rm.val = alu_add(r.val, rm.val, data_size);
+//     operand_write(&rm);
+    
+//     print_asm_2("add", "v", len, &r, &rm);
+//     return len;
+// }
+
+// make_instr_func(add_i2rm_v){
+//     OPERAND imm, rm;
+    
+//     int len = 1;
+//     rm.data_size = data_size; 
+//     len += modrm_rm(eip+1, &rm);
+    
+//     imm.type = OPR_IMM;
+//     imm.addr = eip + len;
+//     imm.data_size = data_size;
+    
+//     operand_read(&rm);
+//     operand_read(&imm);
+//     rm.val = alu_add(imm.val, rm.val, data_size);
+//     operand_write(&rm);
+    
+//     print_asm_2("add", "v", len, &imm, &rm);
+//     return len + data_size / 8;
+// }
 
 make_instr_func(add_i2rm_bv){
     OPERAND imm, rm;
@@ -76,17 +89,17 @@ make_instr_func(add_i2rm_bv){
     return len + 1;
 }
 
-make_instr_func(add_i2a_v){
-    OPERAND imm;
+// make_instr_func(add_i2a_v){
+//     OPERAND imm;
     
-    int len = 1;
-    imm.type = OPR_IMM;
-    imm.data_size = data_size;
-    imm.addr = eip + 1;
+//     int len = 1;
+//     imm.type = OPR_IMM;
+//     imm.data_size = data_size;
+//     imm.addr = eip + 1;
     
-    operand_read(&imm);
-    cpu.eax = alu_add(cpu.eax, sign_ext(imm.val, data_size), 32);
+//     operand_read(&imm);
+//     cpu.eax = alu_add(cpu.eax, sign_ext(imm.val, data_size), 32);
     
-    print_asm_1("add", "", len, &imm);
-    return len + data_size / 8;
-}
+//     print_asm_1("add", "", len, &imm);
+//     return len + data_size / 8;
+// }
