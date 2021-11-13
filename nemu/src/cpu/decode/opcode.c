1b9c2f8d2ad5a8fddd7e33c3aae781e14d67a0e2
tracer-pa-public <tracer@njuics.org>
2021-11-13 11:41:10 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:41:10 up 79 days, 45 min,  4 users,  load average: 2.90, 2.60, 2.54 > unitime 1636774870801 > 381d9424b6b4e826d0e8cda2f0109d1a9f0a0fa

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -16,7 +16,7 @@ instr_func opcode_entry[256] = {
     /* 0x30 - 0x33*/ xor_r2rm_b, xor_r2rm_v, xor_rm2r_b, xor_rm2r_v,
     /* 0x34 - 0x37*/ inv, inv, inv, inv,
     /* 0x38 - 0x3b*/ cmp_r2rm_b, cmp_r2rm_v, cmp_rm2r_b, cmp_rm2r_v,
-    /* 0x3c - 0x3f*/ cmp_i2a_b, cmp_i2_v, inv, inv,
+    /* 0x3c - 0x3f*/ cmp_i2a_b, cmp_i2a_v, inv, inv,
     /* 0x40 - 0x43*/ inc_eax, inc_ecx, inc_edx, inc_ebx,
     /* 0x44 - 0x47*/ inc_esp, inc_ebp, inc_esi, inc_edi,
     /* 0x48 - 0x4b*/ dec_a_l, dec_c_l, dec_d_l, dec_b_l,
