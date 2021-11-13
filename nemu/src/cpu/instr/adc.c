22cf24ae12b9b770cdd5e7ecb03409d13c0832a1
tracer-pa-public <tracer@njuics.org>
2021-11-13 11:17:46 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:17:46 up 79 days, 22 min,  4 users,  load average: 2.67, 2.60, 2.52 > unitime 1636773465907 > 29a36f3643b2a0d0f5f825d0df88b31e22438b6d

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -2,20 +2,30 @@
 /*
 Put the implementations of `adc' instructions here.
 */
-make_instr_func(adc_r2rm_v){
-    OPERAND r, rm;
-    
-    int len = 1;
-    r.data_size = data_size;
-    rm.data_size = data_size;
-    len += modrm_r_rm(eip+1, &r, &rm);
-    
-    operand_read(&r);
-    operand_read(&rm);
-    rm.val = alu_adc(r.val, rm.val, data_size);
-    operand_write(&rm);
-    
-    print_asm_2("adc", "v", len, &r, &rm);
-    
-    return len;
+static void instr_execute_2op()
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_dest.val = alu_adc(opr_src.val, opr_dest.val, data_size);
+    operand_write(&opr_dest);
 }
+
+make_instr_impl_2op(adc, r, rm, v);
+
+// make_instr_func(adc_r2rm_v){
+//     OPERAND r, rm;
+    
+//     int len = 1;
+//     r.data_size = data_size;
+//     rm.data_size = data_size;
+//     len += modrm_r_rm(eip+1, &r, &rm);
+    
+//     operand_read(&r);
+//     operand_read(&rm);
+//     rm.val = alu_adc(r.val, rm.val, data_size);
+//     operand_write(&rm);
+    
+//     print_asm_2("adc", "v", len, &r, &rm);
+    
+//     return len;
+// }
