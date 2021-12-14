fc95a308f404c26c4328e7e155150e26317e0f5c
tracer-pa-public <tracer@njuics.org>
2021-12-14 17:12:19 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:12:19 up 110 days,  6:16,  8 users,  load average: 2.49, 2.48, 2.46 > unitime 1639473139500 > 30a5c1b3b6a76f97fe029766f3fcdaaea2cce6eb

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -19,7 +19,9 @@ void load_sreg(uint8_t sreg)
 	 */
 	 SegReg *segReg = &cpu.segReg[sreg];
 	 laddr_t addr = cpu.gdtr.base + (uint32_t)(segReg->index & 0x1fff);
-	 SegDesc segDesc = (SegDesc)laddr_read(addr, sizeof(SegDesc));
+	 SegDesc segDesc;
+	 segDesc.val[0] = laddr_read(addr, 4);
+	 segDesc.val[1] = laddr_read(addr + 4, 4);
 	 
 	 //load invisible part from segDesc
 	 segReg->base = segDesc.base_15_0 + (segDesc.base_23_16 << 16) + (segDesc.base_31_24 << 24);
