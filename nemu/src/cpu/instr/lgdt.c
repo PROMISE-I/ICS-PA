ad522ec5050c901126967ab21538036d4622e9bd
tracer-pa-public <tracer@njuics.org>
2021-12-14 20:32:40 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:32:40 up 110 days,  9:36, 13 users,  load average: 2.38, 2.63, 2.61 > unitime 1639485160400 > dc941329b69f79b2c4a38e17e38228f495b1ac6

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,33 +2,34 @@
 /*
 Put the implementations of `lgdt' instructions here.
 */
-#ifdef IA32_SEG
+
 make_instr_func(lgdt)
 {
-    OPERAND limit, base;
-    
-    limit.type = OPR_IMM;
-    limit.data_size = 16;
-    limit.addr = eip;
-    
-    base.type = OPR_IMM;
-    base.data_size = 32;
-    base.addr = eip + 2;
-    
-    operand_read(&limit);
-    operand_read(&base);
-    
-    cpu.gdtr.limit = limit.val;
-    if(data_size == 32) {
-        cpu.gdtr.base = base.val;
-    }
-    else {
-        cpu.gdtr.base = (base.val & 0xffffff);
-    }
-    print_asm_2("lgdt", "", 6, &limit, &base);
-    //printf("base: %x, limit: %x\n", cpu.gdtr.base, cpu.gdtr.limit);
-    //fflush(stdout);
-    
-    return 6;
-}
+#ifdef IA32_SEG
+        OPERAND limit, base;
+        
+        limit.type = OPR_IMM;
+        limit.data_size = 16;
+        limit.addr = eip;
+        
+        base.type = OPR_IMM;
+        base.data_size = 32;
+        base.addr = eip + 2;
+        
+        operand_read(&limit);
+        operand_read(&base);
+        
+        cpu.gdtr.limit = limit.val;
+        if(data_size == 32) {
+            cpu.gdtr.base = base.val;
+        }
+        else {
+            cpu.gdtr.base = (base.val & 0xffffff);
+        }
+        print_asm_2("lgdt", "", 6, &limit, &base);
+        //printf("base: %x, limit: %x\n", cpu.gdtr.base, cpu.gdtr.limit);
+        //fflush(stdout);
+        
+        return 6;
 #endif
+}
