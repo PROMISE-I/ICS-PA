84a3c6391456e69ef3a26c22572ed4aa2ca61b26
tracer-pa-public <tracer@njuics.org>
2021-12-14 19:56:51 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:56:51 up 110 days,  9:01, 13 users,  load average: 2.30, 2.57, 2.54 > unitime 1639483011747 > d696a84d685523ff76c834497a4dd663ff7a86eb

--- nemu/include/cpu/instr/or.h
+++ nemu/include/cpu/instr/or.h
@@ -7,4 +7,5 @@ make_instr_func(or_r2rm_b);
 make_instr_func(or_r2rm_v);
 make_instr_func(or_rm2r_b);
 make_instr_func(or_rm2r_v);
+make_instr_func(or_i2rm_bv);
 #endif
--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -6,7 +6,7 @@ static void instr_execute_2op()
 {
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_dest.val = alu_or(opr_src.val, opr_dest.val, data_size);
+    opr_dest.val = alu_or(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
     operand_write(&opr_dest);
 }
 
@@ -14,6 +14,7 @@ make_instr_impl_2op(or, r, rm, b)
 make_instr_impl_2op(or, r, rm, v)
 make_instr_impl_2op(or, rm, r, b)
 make_instr_impl_2op(or, rm, r, v)
+make_instr_impl_2op(or, i, rm, bv)
 
 // make_instr_func(or_r2rm_b){
 //     OPERAND r, rm;
