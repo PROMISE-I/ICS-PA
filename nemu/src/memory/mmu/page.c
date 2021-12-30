618777e49e2ff67bb91209dc3b93f54c69a977c1
tracer-pa-public <tracer@njuics.org>
2021-12-30 17:09:14 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:09:14 up 126 days,  6:13, 13 users,  load average: 2.65, 2.63, 2.62 > unitime 1640855354838 > 76b08611cd5d3507a238f0147a2b0c5eab76fee

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -8,7 +8,7 @@ paddr_t page_translate(laddr_t laddr)
     printf("cpu.cr3: %x; laddr: %x\n", cpu.cr3.PDBR, laddr);
 	paddr_t PDEpaddr = (cpu.cr3.PDBR << 12) + ((laddr >> 22) & 0x3ff);
 	//printf("PDEpaddr: %x\n", PDEpaddr);
-	printf("eip: %x\n", cpu.eip);
+	printf("eip: %x\n", cpu.eip << 12);
 	PDE pde;
 	pde.val = paddr_read(PDEpaddr, 4);
 	assert(pde.present == 1);
