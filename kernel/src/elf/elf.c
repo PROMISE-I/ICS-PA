68cdef075c5cb7848468c2d2e41d05999ebb8bcc
tracer-pa-public <tracer@njuics.org>
2021-12-30 16:48:31 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux c885a299b3ee 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:48:31 up 126 days,  5:52, 13 users,  load average: 2.69, 2.55, 2.51 > unitime 1640854111669 > 72d96710195c18c0fda3b34e67b9db74eda716

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,7 +41,11 @@ uint32_t loader()
 
 			// remove this panic!!!
 			//panic("Please implement the loader");
+#ifdef IA32_PAGE
             uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+#else
+            uint32_t paddr = ph->vaddr;
+#endif
             // printf("paddr in load: %x\n", paddr);
             // fflush(stdout);
 /* TODO: copy the segment from the ELF file to its proper memory area */
