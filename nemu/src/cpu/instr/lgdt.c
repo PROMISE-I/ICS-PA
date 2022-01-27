b1b4b2fb0bca6560c04119fe92c3778ebaa5751d
tracer-pa-public <tracer@njuics.org>
2022-01-27 16:54:25 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 16:54:25 up  7:21,  1 user,  load average: 0.00, 0.01, 0.00 > unitime 1643273665351 > 8cf4ce2db99df5673a3c7522d54f118ae0d24ae8

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -6,15 +6,21 @@ Put the implementations of `lgdt' instructions here.
 make_instr_func(lgdt)
 {
 #ifdef IA32_SEG
-        OPERAND limit, base;
+        int len = 1;
+        OPERAND rm, limit, base;
         
+        rm.data_size = 32;
+        len += modrm_rm(eip+1, &rm);
+
+        operand_read(&rm);
+
         limit.type = OPR_IMM;
         limit.data_size = 16;
-        limit.addr = eip;
+        limit.addr = rm.val;
         
         base.type = OPR_IMM;
         base.data_size = 32;
-        base.addr = eip + 2;
+        base.addr = rm.val + 2;
         
         operand_read(&limit);
         operand_read(&base);
@@ -27,8 +33,8 @@ make_instr_func(lgdt)
             cpu.gdtr.base = (base.val & 0xffffff);
         }
         print_asm_2("lgdt", "", 6, &limit, &base);
-        //printf("base: %x, limit: %x\n", cpu.gdtr.base, cpu.gdtr.limit);
-        //fflush(stdout);
+        printf("base: %x, limit: %x\n", cpu.gdtr.base, cpu.gdtr.limit);
+        fflush(stdout);
 #endif        
-        return 6;
+        return len;
 }
