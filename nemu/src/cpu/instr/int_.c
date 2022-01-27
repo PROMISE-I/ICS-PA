c3a31ab9c3da8d15bb294e0d472f335c79eed612
tracer-pa-public <tracer@njuics.org>
2022-01-27 10:48:50 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 10:48:50 up  1:15,  1 user,  load average: 0.08, 0.05, 0.00 > unitime 1643251730330 > 3991557026d0942e5b63819090d280ec11ea8a4a

--- nemu/src/cpu/instr/int_.c
+++ nemu/src/cpu/instr/int_.c
@@ -6,3 +6,15 @@ Put the implementations of `int' instructions here.
 
 Special note for `int': please use the instruction name `int_' instead of `int'.
 */
+make_instr_func(int_)
+{
+    OPERAND imm;
+
+    imm.type = OPR_IMM;
+    imm.data_size = 8;
+    imm.addr = eip + 1;
+    
+    operand_read(&imm);
+    
+    raise_sw_intr(0x80);
+}
