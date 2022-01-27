ed18c9cb0b0722c7be556040b69996e7183a7c82
tracer-pa-public <tracer@njuics.org>
2022-01-27 10:32:55 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 10:32:55 up 59 min,  1 user,  load average: 0.04, 0.03, 0.00 > unitime 1643250775509 > 1fb44be243ca10202771eaa6ff799ab1183f9364

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,3 +2,38 @@
 /*
 Put the implementations of `lidt' instructions here.
 */
+make_instr_func(lidt)
+{
+    uint32_t len = 1;
+    OPERAND mem, base, limit;
+    
+    mem.data_size = 32;
+    len += modrm_rm(eip+1, &mem);
+
+    //case for mod 11 -> register
+    operand_read(&mem);
+    if (mem.type = OPR_REG) {
+        mem.addr = mem.val;    
+    }
+
+    limit.type = OPR_MEM;
+    limit.data_size = 16;
+    limit.addr = mem.addr;
+    
+    base.type = OPR_MEM;
+    base.data_size = 32;
+    base.addr = mem.addr + 2;
+
+    operand_read(&limit);
+    operand_read(&base);
+
+    cpu.idtr.limit = limit.val;
+    if(data_size == 32) {
+            cpu.idtr.base = base.val;
+        }
+    else {
+        cpu.idtr.base = (base.val & 0xffffff);
+    }
+
+    return len;
+}
