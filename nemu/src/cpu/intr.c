f280fd1a64d3f876e4aa2628ab170c1daa7bab4a
tracer-pa-public <tracer@njuics.org>
2022-01-27 12:03:21 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 12:03:21 up  2:30,  1 user,  load average: 0.20, 0.14, 0.08 > unitime 1643256201278 > 6c0294215ecb7cf39e6d942484b0b605a4083c4

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -12,7 +12,7 @@ void raise_intr(uint8_t intr_no)
     //find the IDT entry using 'inrt_no'
     printf("cpu.idtr: 0x%x, itro_no: 0x%x\n",cpu.idtr.base,intr_no);
     GateDesc *entry = (GateDesc *)cpu.idtr.base + intr_no;
-    printf("entry: 0x%x\n", entry);
+    printf("entry: 0x%x\n", (uint32_t)entry);
     //Clear IF if it is an interrupt
     if (intr_no >= 1000) { //there is something wrong, intr_no in do_irq.S begins with 1000, while that begins with 32 in idt.c 
         printf("it is a interrupt!\n");
@@ -20,7 +20,7 @@ void raise_intr(uint8_t intr_no)
     }
     fflush(stdout);
     //Set cs:eip to the entry of interrupt handler
-    cpu.cs.val = entry->segment;
+    cpu.cs.val = entry->selector;
     cpu.eip = (entry->offset_15_0 & 0xffff) + ((entry->offset_31_16 << 16) & 0xffff0000);
     //Reload cs with load_sreg()
     load_sreg(1);
