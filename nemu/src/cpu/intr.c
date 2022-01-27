1af3bfa1fb3e16f3ad10436460fe764b7bc0dfb7
tracer-pa-public <tracer@njuics.org>
2022-01-27 18:29:16 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 18:29:16 up  8:56,  1 user,  load average: 0.01, 0.04, 0.00 > unitime 1643279356158 > 60b3a68bbb93bccb31603c274fdcc9a9719ea9f3

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,6 +7,7 @@ void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
     //Push eflags,cs,eip
+    printf("eip: 0x%x\n", cpu.eip);
     make_instr_func(push_eflags);
     make_instr_func(push_cs);
     make_instr_func(push_eip);
