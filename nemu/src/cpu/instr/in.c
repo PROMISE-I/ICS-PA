538e44911b3fb23295a13b45a863d97938ffeb28
tracer-pa-public <tracer@njuics.org>
2022-01-30 13:22:53 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 13:22:53 up  1:01,  1 user,  load average: 0.04, 0.06, 0.02 > unitime 1643520173976 > 851bf5838801d4398004503bd9188a1e8eae475d

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -6,6 +6,6 @@ Put the implementations of `in' instructions here.
 make_instr_func(in_b)
 {
     int len = 1;
-    cpu.eax._8[0] = read_io_port(cpu.edx._16, 2);
+    cpu.gpr[0]._8[0] = read_io_port(cpu.gpr[2]._16, 2);
     return len;
 }
