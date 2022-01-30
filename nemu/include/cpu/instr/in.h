a20d9ba5e08ad7d2f601cdfdcef45fa35165cfe2
tracer-pa-public <tracer@njuics.org>
2022-01-30 13:17:42 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 13:17:42 up 55 min,  1 user,  load average: 0.21, 0.13, 0.04 > unitime 1643519862191 > 4a3d36b252af65256bf5aaeae11fbee5ba84162a

--- nemu/include/cpu/instr/in.h
+++ nemu/include/cpu/instr/in.h
@@ -3,5 +3,5 @@
 /*
 Put the declarations of `in' instructions here.
 */
-
+make_instr_func(in_b);
 #endif
--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -3,3 +3,9 @@
 /*
 Put the implementations of `in' instructions here.
 */
+make_instr_func(in_b)
+{
+    int len = 1;
+    cpu.eax._8[0] = read_io_port(cpu.edx._16, 2);
+    return len;
+}
