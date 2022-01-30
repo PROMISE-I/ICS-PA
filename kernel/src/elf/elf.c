0cc4e6f20c28cf9680a7af9275f78d174c01ef5d
tracer-pa-public <tracer@njuics.org>
2022-01-30 17:06:26 +0800

    > compile nemu testcase kernel > id 201250185 > user pa201250185 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 17:06:26 up  4:44,  1 user,  load average: 0.06, 0.15, 0.07 > unitime 1643533586390 > 516fd4adb69dcabb3c548b31af080f38b2e54

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -2,7 +2,7 @@
 #include "memory.h"
 #include "string.h"
 
-#include <stdio.h>
+//#include <stdio.h>
 
 #include <elf.h>
 
@@ -30,7 +30,7 @@ uint32_t loader()
 	elf = (void *)0x0;
 	Log("ELF loading from ram disk.");
 #endif
-    fflush(stdout);
+    BREAK_POINT;
 	/* Load each program segment */
 	ph = (void *)elf + elf->e_phoff;
 	eph = ph + elf->e_phnum;
